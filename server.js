1899989d50b271b6a455ce00c263e9ca830c08eb
deflk786 <secdefmodlk@gmail.com>
2024-11-21 07:26:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -46,9 +46,4 @@ app.use('/backend',backendRouter);
 
 
 
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
